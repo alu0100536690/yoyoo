fae87827de7a145c0aa907ddac5364b7f80fa394
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-12-02 14:44:36 +0000

    desplegando en heroku

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
 // importar
-var express = require('express');
+    var express = require('express');
+    
+    // instanciar
+    var app = express();
+    var path = require('path');
+    
+    var exec = require('child_process').exec; 
 
-// instanciar
-var app = express();
-var path = require('path');
-
-var exec = require('child_process').exec;
-var  expressEJS = require('express-ejs-layouts')
-
+    
    // ruteo
 
-app.set('port', process.env.PORT || 8080);
-app.use(expressEJS);
-app.use(express.static(path.join(__dirname,'gh-pages')));
-
-app.get('/', function(request, response) {
- response.send('index');
-});
+   app.use(express.static(path.join(__dirname,'./gh-pages')));
 
 
-/*app.post('/sync', function(request, response) {
+   app.get('/', function(request, response) {
+     response.send('index');  
+   });
+
+
+app.post('/sync', function(request, response) {
         function puts(error, stdout, stderr) {
            console.log("Salida:"+stdout);
            if(error) console.log("Error:"+error);
          }
 
-   exec('git clone https://github.com/ULL-ESIT-SYTW-1617/nueva-funcionalidad-para-el-paquete-npm-plugins-merquililycony');
-   exec("git pull https://github.com/ULL-ESIT-SYTW-1617/nueva-funcionalidad-para-el-paquete-npm-plugins-merquililycony.git  master", puts);
+                           
+   exec('git clone https://github.com/alu0100536690/yoyoo.git'); 
+   exec("git pull https://github.com/alu0100536690/yoyoo.git  master", puts);
    response.redirect('/');
-});*/
+}); 
 
-
+     
     // escuchar
    // app.listen(8080);
+   app.set('port', process.env.PORT || 8080);
 
 
-//heroku git:remote -a heroku-project-name
+//heroku git:remote -a heroku-project-name    
 app.listen(app.get('port'), function() {
   console.log('Servidor escuchando en el puerto:'+app.get('port'));
 });
